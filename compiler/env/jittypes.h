b65a3574948ddda75c8abdbbd234a2e354bb1d3b
midronij <jackie.midroni@ibm.com>
2024-03-20 18:42:58 -0400

    Use inline assembly instead of __lwsync() builtin on AIX
    Signed-off-by: midronij <jackie.midroni@ibm.com>

--- compiler/env/jittypes.h
+++ compiler/env/jittypes.h
@@ -144,7 +144,7 @@ typedef struct TR_InlinedCallSite
       #include <builtins.h>
       #endif /* __cplusplus */
       #define FLUSH_MEMORY(smp) if( smp ) __lwsync();
-   #elif defined(LINUX)
+   #else
       #define FLUSH_MEMORY(smp) if( smp ) __asm__("lwsync");
    #endif
 #endif
