4232e35f651b5b024c89100f47010434cfd26bae
Peter Shipton <Peter_Shipton@ca.ibm.com>
2025-12-19 12:03:03 -0500

    Revert "Add optional spin/sleep in park()"
    This reverts commit 5a450bbaad7dc31e87ce121ed77db4cc86cd4b37.

--- include_core/thrtypes.h
+++ include_core/thrtypes.h
@@ -151,13 +151,6 @@ typedef struct J9ThreadLibrary {
 #if defined(OSX)
 	clock_serv_t clockService;
 #endif /* defined(OSX) */
-#if defined(OMR_THR_YIELD_ALG)
-	uintptr_t parkPolicy;
-	uintptr_t parkSleepMultiplier;
-	uintptr_t parkSleepTime;
-	uintptr_t parkSpinCount;
-	uintptr_t parkSleepCount;
-#endif /* defined(OMR_THR_YIELD_ALG) */
 } J9ThreadLibrary;
 
 /*
